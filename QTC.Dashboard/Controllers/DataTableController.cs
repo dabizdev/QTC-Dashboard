7a869abe2ad55f0d3d64c932179d5d318c4a8dbe
AdrianSLopez <41001.adrian@gmail.com>
2023-02-09 15:18:22 -0800

    Add controller for table

---
+++ QTC.Dashboard/Controllers/DataTableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QTC_Smart_Dashboard.Controllers
+{
+    public class DataTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
