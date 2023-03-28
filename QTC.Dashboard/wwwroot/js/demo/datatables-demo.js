5efcaf2c64a3d7934ef9567ae6ebbb61f573f138
Nep2un <jupi2erinspace@gmail.com>
2023-03-27 18:29:36 -0700

    Able to view error modal on every table page.

--- QTC.Dashboard/wwwroot/js/demo/datatables-demo.js
+++ QTC.Dashboard/wwwroot/js/demo/datatables-demo.js
@@ -4,4 +4,18 @@ $(document).ready(function () {
         /* Order severity at column 5 by ascending */
         "order": [5, 'asc']
     });
+
+
+    /*Display error info to modal when on view click*/
+    $('#dataTable tbody').on('click', '.view-error', function () {
+        var data = table.row(this).data();
+
+        /*Display error*/
+        $("#modal-title").text(data[0] + " - " + data[5]);
+        $("#modal-title").prepend("<i class='fa fa-eye'></i> ")
+
+        $("#modal-message").text(data[8]);
+
+        $("#modal-date").text(data[9]);
+    });
 });
